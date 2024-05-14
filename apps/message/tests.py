e54b64c76ee3f96f172eebefe36e136a2dccd9e3
fsj <fsjasd123456@gmail.com>
2024-05-14 08:32:48 +0800

    2024-5-13 清理源码、dev-节点页

--- apps/message/tests.py
+++ apps/message/tests.py
@@ -14,23 +14,3 @@ class MessageTest(TestCase):
         Message.objects.create(title="测试", content="测试" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),created_at=datetime.datetime.now(),updated_at=datetime.datetime.now())
         result = get_message_list({})
         print(result.getvalue())
-
-
-
-"""        User.objects.create(
-            userName='test',
-            realName='test',
-            email='1101658312@qq.com',
-            password='123456',
-            passwordSalt='1234567',
-            disable=False,
-            )
-        User.objects.create(
-            userName='test1',
-            realName='test1',
-            email='3072864687@qq.com',
-            password='123456',
-            passwordSalt='123456',
-            disable=False,
-            )
-        send_email(MessageBody("测试", "测试" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))"""
