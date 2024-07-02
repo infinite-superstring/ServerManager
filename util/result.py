a1928250d96e4b9387a0125d9925f079ad1ac66a
1101658312@qq.com <1101658312@qq.com>
2024-07-02 11:42:01 +0800

    统一响应结果

---
+++ util/result.py
@@ -0,0 +1,31 @@
+from util.Response import ResponseJson
+
+
+def success(data='None', msg='success', http_code=200, result_code=0):
+    return ResponseJson(status=http_code, data={
+        'status': result_code,
+        'msg': msg,
+        'data': data
+    })
+
+
+def error(msg='error', http_code=200, result_code=1):
+    return ResponseJson(
+        status=http_code,
+        data={
+            'status': result_code,
+            'msg': msg,
+            'data': ''
+        }
+    )
+
+
+def api_error(msg='error', http_code=200, result_code=-1):
+    return ResponseJson(
+        status=http_code,
+        data={
+            'status': result_code,
+            'msg': msg,
+            'data': ''
+        }
+    )
