df0811c1e25ed9ebe0a484767fe6c092a448099c
fsj <fsjasd123456@gmail.com>
2024-07-10 08:59:34 +0800

    调整服务器认证机制

--- apps/auth/api/node_auth.py
+++ apps/auth/api/node_auth.py
@@ -6,7 +6,9 @@ from apps.node_manager.utils.nodeUtil import verify_node_token
 from util.Request import RequestLoadJson, getClientIp
 from util.Response import ResponseJson
 from util.logger import Log
+from util.passwordUtils import encrypt_password
 
+config = apps.get_app_config('setting').get_config
 
 def node_auth(req: HttpRequest):
     """
@@ -22,23 +24,27 @@ def node_auth(req: HttpRequest):
     node_name = req_json.get("node_name")  # 节点名
     node_token = req_json.get("node_token")  # 节点Token
     node_ip = getClientIp(req)  # 节点认证时IP地址
-    server_token = req_json.get("server_token")  # 服务器Token
-    if not (node_name or node_token or node_ip or server_token):
+    if not (node_name or node_token or node_ip):
         return ResponseJson({"status": -1, "msg": "参数不完整"})
     if not Node.objects.filter(name=node_name).exists():
         return ResponseJson({"status": 0, "msg": "节点不存在"})
     node = Node.objects.get(name=node_name)
-    if (
-        node and
-        verify_node_token(node, node_token) and
-        server_token == apps.get_app_config('setting').get_config().base.server_token
-    ):
+    if (node and verify_node_token(node, node_token)):
         req.session["node_uuid"] = str(node.uuid)
         req.session["node_name"] = node.name
         req.session["auth_method"] = "Node Auth"
         req.session.set_expiry = 0
         if not Node_BaseInfo.objects.filter(node=node).exists():
             Node_BaseInfo.objects.create(node=node, node_version="")
-        return ResponseJson({"status": 1, "msg": "节点认证成功"})
+
+        hashed_token, salt = encrypt_password(config().base.server_token)
+
+        return ResponseJson({"status": 1, "data": {
+            'server_token': {
+                'hash': str(hashed_token),
+                'salt': salt
+            },
+            'server_name': config().base.website_name,
+        }})
     else:
         return ResponseJson({"status": 0, "msg": "节点认证失败"})
