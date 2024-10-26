62271790bd123e7df2deaa12b9b62d8686e2d10f
fsj <fsjasd123456@gmail.com>
2024-10-26 08:17:14 +0800

    优化安装体验

--- setup.sh
+++ setup.sh
@@ -47,6 +47,7 @@ install_docker() {
     # 安装 Docker Compose
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
 
 # 运行 Docker Compose
